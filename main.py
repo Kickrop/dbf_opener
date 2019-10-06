7400ca01fec55930ffd4a1abaa3d4a64c0252ead
den <kickrop@gmail.com>
2019-10-06 15:11:59 +0300

    renamed output file

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 import dbf
 with dbf.Table('allforms.dbf') as table:
-    dbf.export(table, 'junk.csv')
+    dbf.export(table, 'output.csv')
 
---
+++ sum_fo.py
@@ -0,0 +1,17 @@
+import pandas as pd
+#from simpledbf import Dbf5
+df = pd.read_csv('junk.csv')
+
+#df.drop(["b'col_32', 6",], axis=1)
+
+#drop columns with zeros
+df = df.drop(df.columns[df.apply(lambda col: col.sum() == 0)], axis=1)
+
+no_place = df.groupby(
+    ["(b'cur_year', 8)", "(b'razdel', 6)", "(b'num_of_row', 10)", "(b'num_of_sub', 10)"]).sum()
+
+no_place.to_csv("summa.csv")
+#print(df.info())
+#print(df["(b'num_of_sub', 10)"].sum())
+#print(no_place.head())
+#print(df.head())
