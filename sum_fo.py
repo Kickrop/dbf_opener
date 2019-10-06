77d77e27fca97a97721e598783afa35efc847fe3
den <kickrop@gmail.com>
2019-10-06 17:42:31 +0300

    update

--- sum_fo.py
+++ sum_fo.py
@@ -10,7 +10,8 @@ df = df.drop(df.columns[df.apply(lambda col: col.sum() == 0)], axis=1)
 no_place = df.groupby(
     ["(b'cur_year', 8)", "(b'razdel', 6)", "(b'num_of_row', 10)", "(b'num_of_sub', 10)"]).sum()
 
-no_place.to_csv("summa.csv")
+
+#no_place.to_csv("summa.csv")
 #print(df.info())
 #print(df["(b'num_of_sub', 10)"].sum())
 #print(no_place.head())
